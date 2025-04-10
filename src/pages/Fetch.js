3c707884c253799f2dccb2842115b3aec7e173aa
Jeffery Lyle <J.Lyle2239@gmail.com>
2025-04-10 09:57:29 -0400

    hello

--- src/pages/Fetch.js
+++ src/pages/Fetch.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../style/fetch.css'
+import BACKEND_URL from '../config';
 
 const Customers = () => {
   const [customers, setCustomers] = useState([]);
